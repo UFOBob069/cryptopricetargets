2675ef553f2277e6284833d4729ed00c58d60fd2
UFOBob069 <UFOBob069@yahoo.com>
2025-01-19 11:21:01 -0600

    Fix Floating Achievement Cards responsiveness for mobile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
- 
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/no-unescaped-entities': 'warn',
+    '@next/next/no-html-link-for-pages': 'error'
+  }
+} 
